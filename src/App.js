cdd4456b3522e785c99ba4bf6c62b3f2724f4e7d
DnyaneshwarShiv <shivbhaktadnyaneshwar.1@gmail.com>
2020-12-31 17:29:38 +0530

    changes for routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component';
+import {Route} from 'react-router-dom';
 
+const HatsPage= ()=>(
+  <div>
+    <h1>HATS Page</h1>
+  </div>
+)
+// const TopicsPage= ()=>(
+//   <div>
+//     <h1>Topic Page</h1>
+//   </div>
+// )
+// const TopicDetailsPage= ()=>(
+//   <div>
+//     <h1>Topic details Page</h1>
+//   </div>
+// )
 function App() {
   return (
     <div>
-     <HomePage></HomePage>
+     <Route exact path='/' component={HomePage}></Route>
+     <Route exact path='/hats' component={HatsPage}></Route>
     </div>
   );
 }
